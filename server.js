930b1c1db414d977bd470932dd300d33cae5d079
William McLaughlin <williammclaughlin@Williams-MacBook-Pro.local>
2019-01-30 19:01:08 -0600

    amended code

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
-
 // Connect Handlebars to our Express app
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
